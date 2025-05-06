6304625cf2a44c8f21661c2a3f74b893ce4faf25
Abdulaziz <s202023480@kfupm.edu.sa>
2025-05-06 11:13:45 +0300

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');  // أضف هذه السطر
 require('dotenv').config();
 
 const app = express();
@@ -17,6 +18,14 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/user'));
 app.use('/api/items', require('./routes/items'));
 app.use('/api/messages', require('./routes/message'));
+
+// Serve React build folder
+app.use(express.static(path.join(__dirname, "../frontend/kfupmmarket/build"))); // أضف هذه السطر
+
+// Catch-all route to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/kfupmmarket/build/index.html"));
+});
 
 // Connect DB & Start Server
 mongoose.connect(process.env.MONGO_URI)
